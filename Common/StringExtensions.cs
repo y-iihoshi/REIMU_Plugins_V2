7911a1e61f10488925eafbea2c1d85b19dca779b
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-12 05:13:07 +0900

    Common: Add StringExtensions class

---
+++ Common/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public static class StringExtensions
+    {
+        public static string ToCStr(this string str)
+        {
+            return str.Contains('\0') ? str : str + '\0';
+        }
+
+        public static string Convert(this string str, Encoding src, Encoding dst)
+        {
+            return dst.GetString(Encoding.Convert(src, dst, src.GetBytes(str)));
+        }
+    }
+}
