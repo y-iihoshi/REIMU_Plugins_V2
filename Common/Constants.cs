b277e8b67d46a4d131d927530235c34ad7c1356a
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-17 05:35:24 +0900

    Fix to avoid SA1602

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -13,7 +13,14 @@ namespace ReimuPlugins.Common
     /// </summary>
     public enum Revision
     {
+        /// <summary>
+        /// Revision 1.
+        /// </summary>
         Rev1 = 1,
+
+        /// <summary>
+        /// Revision 2.
+        /// </summary>
         Rev2
     }
 
@@ -22,13 +29,44 @@ namespace ReimuPlugins.Common
     /// </summary>
     public enum ErrorCode
     {
+        /// <summary>
+        /// The function is not implemented.
+        /// </summary>
         NoFunction = -1,
+
+        /// <summary>
+        /// Finished successfully.
+        /// </summary>
         AllRight,
+
+        /// <summary>
+        /// The format is not supported.
+        /// </summary>
         NotSupport,
+
+        /// <summary>
+        /// An error occurred while reading a file.
+        /// </summary>
         FileReadError,
+
+        /// <summary>
+        /// An error occurred while writing a file.
+        /// </summary>
         FileWriteError,
+
+        /// <summary>
+        /// Could not allocate memory.
+        /// </summary>
         NoMemory,
+
+        /// <summary>
+        /// An unknown error occurred.
+        /// </summary>
         UnknownError,
+
+        /// <summary>
+        /// Cancelled in the edit/config dialog.
+        /// </summary>
         DialogCanceled
     }
 
@@ -37,8 +75,22 @@ namespace ReimuPlugins.Common
     /// </summary>
     public enum TextAlign
     {
+        /// <summary>
+        /// Text is left-aligned.
+        /// <remarks>The value must be equal to <c>LCVFMT_LEFT</c> of Win32 API.</remarks>
+        /// </summary>
         Left = 0,
+
+        /// <summary>
+        /// Text is right-aligned.
+        /// <remarks>The value must be equal to <c>LCVFMT_RIGHT</c> of Win32 API.</remarks>
+        /// </summary>
         Right,
+
+        /// <summary>
+        /// Text is centered.
+        /// <remarks>The value must be equal to <c>LCVFMT_CENTER</c> of Win32 API.</remarks>
+        /// </summary>
         Center
     }
 
@@ -47,9 +99,24 @@ namespace ReimuPlugins.Common
     /// </summary>
     public enum SortType
     {
+        /// <summary>
+        /// String sort.
+        /// </summary>
         String = 0,
+
+        /// <summary>
+        /// Integer sort.
+        /// </summary>
         Number,
+
+        /// <summary>
+        /// Floating point sort.
+        /// </summary>
         Float,
+
+        /// <summary>
+        /// Hexadecimal sort.
+        /// </summary>
         Hex
     }
 
@@ -58,14 +125,49 @@ namespace ReimuPlugins.Common
     /// </summary>
     public enum SystemInfoType
     {
+        /// <summary>
+        /// Not any system information.
+        /// </summary>
         String = 0,
+
+        /// <summary>
+        /// The file path.
+        /// </summary>
         Path,
+
+        /// <summary>
+        /// The directory path.
+        /// </summary>
         Directory,
+
+        /// <summary>
+        /// The filename except the extension.
+        /// </summary>
         Title,
+
+        /// <summary>
+        /// The filename extension.
+        /// </summary>
         Extension,
+
+        /// <summary>
+        /// Create time of the file.
+        /// </summary>
         CreateTime,
+
+        /// <summary>
+        /// Last access time of the file.
+        /// </summary>
         LastAccessTime,
+
+        /// <summary>
+        /// Last write time of the file.
+        /// </summary>
         LastWriteTime,
+
+        /// <summary>
+        /// File size (in bytes).
+        /// </summary>
         FileSize
     }
 
