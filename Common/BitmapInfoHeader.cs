488f3971976d085e1243ec1f84172a90c1f74192
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-17 16:03:46 +0900

    Fix to avoid SA1400

--- Common/BitmapInfoHeader.cs
+++ Common/BitmapInfoHeader.cs
@@ -18,56 +18,56 @@ namespace ReimuPlugins.Common
         /// <summary>
         /// The number of bytes required by the structure.
         /// </summary>
-        uint size;
+        public uint size;
 
         /// <summary>
         /// The width of the bitmap, in pixels.
         /// </summary>
-        int width;
+        public int width;
 
         /// <summary>
         /// The height of the bitmap, in pixels.
         /// </summary>
-        int height;
+        public int height;
 
         /// <summary>
         /// The number of planes for the target device.
         /// </summary>
-        ushort planes;
+        public ushort planes;
 
         /// <summary>
         /// The number of bits-per-pixel.
         /// </summary>
-        ushort bitCount;
+        public ushort bitCount;
 
         /// <summary>
         /// The type of compression for a compressed bottom-up bitmap (top-down DIBs cannot be compressed).
         /// </summary>
-        uint compression;
+        public uint compression;
 
         /// <summary>
         /// The size, in bytes, of the image.
         /// </summary>
-        uint sizeImage;
+        public uint sizeImage;
 
         /// <summary>
         /// The horizontal resolution, in pixels-per-meter, of the target device for the bitmap.
         /// </summary>
-        int xPelsPerMeter;
+        public int xPelsPerMeter;
 
         /// <summary>
         /// The vertical resolution, in pixels-per-meter, of the target device for the bitmap.
         /// </summary>
-        int yPelsPerMeter;
+        public int yPelsPerMeter;
 
         /// <summary>
         /// The number of color indexes in the color table that are actually used by the bitmap.
         /// </summary>
-        uint clrUsed;
+        public uint clrUsed;
 
         /// <summary>
         /// The number of color indexes that are required for displaying the bitmap.
         /// </summary>
-        uint clrImportant;
+        public uint clrImportant;
     }
 }
