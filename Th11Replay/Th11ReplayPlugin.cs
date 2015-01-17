afa79ba82ba73569d76895211da920dcf25ac19a
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-17 15:57:50 +0900

    Th11Replay: Suppress SA16xx

--- Th11Replay/Th11ReplayPlugin.cs
+++ Th11Replay/Th11ReplayPlugin.cs
@@ -5,14 +5,13 @@
 //-----------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using System.Reflection;
 using ReimuPlugins.Common;
 using RGiesecke.DllExport;
 using IO = System.IO;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "Reviewed.")]
 
 namespace ReimuPlugins.Th11Replay
 {
