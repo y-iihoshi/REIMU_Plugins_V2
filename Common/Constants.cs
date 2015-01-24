241515e8eb23d82efe97285ab33eed1aee540f3b
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-24 21:11:59 +0900

    Common: Move the Enc class to a new file

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -5,7 +5,6 @@
 //-----------------------------------------------------------------------
 
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:FileHeaderFileNameDocumentationMustMatchTypeName", Justification = "Reviewed.")]
 
@@ -177,30 +176,5 @@ namespace ReimuPlugins.Common
         /// File size (in bytes).
         /// </summary>
         FileSize
-    }
-
-    /// <summary>
-    /// Contains read-only instances of <see cref="Encoding"/> class for convenience.
-    /// </summary>
-    public static class Enc
-    {
-        /// <summary>
-        /// Initializes static members of the <see cref="Enc"/> class.
-        /// </summary>
-        static Enc()
-        {
-            CP932 = Encoding.GetEncoding(932);
-            UTF8 = Encoding.UTF8;
-        }
-
-        /// <summary>
-        /// Gets the code page 932 encoding.
-        /// </summary>
-        public static Encoding CP932 { get; private set; }
-
-        /// <summary>
-        /// Gets the UTF-8 encoding.
-        /// </summary>
-        public static Encoding UTF8 { get; private set; }
     }
 }
---
+++ Common/Enc.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Enc.cs" company="None">
+//     (c) 2015 IIHOSHI Yoshinori
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ReimuPlugins.Common
+{
+    using System.Text;
+
+    /// <summary>
+    /// Contains read-only instances of <see cref="Encoding"/> class for convenience.
+    /// </summary>
+    public static class Enc
+    {
+        /// <summary>
+        /// Initializes static members of the <see cref="Enc"/> class.
+        /// </summary>
+        static Enc()
+        {
+            CP932 = Encoding.GetEncoding(932);
+            UTF8 = Encoding.UTF8;
+        }
+
+        /// <summary>
+        /// Gets the code page 932 encoding.
+        /// </summary>
+        public static Encoding CP932 { get; private set; }
+
+        /// <summary>
+        /// Gets the UTF-8 encoding.
+        /// </summary>
+        public static Encoding UTF8 { get; private set; }
+    }
+}
