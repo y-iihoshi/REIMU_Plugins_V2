3166fd9f5c02d1c5a207a9d112a907fdd4df5edc
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-25 03:59:54 +0900

    Common: Add the Utils class

---
+++ Common/Utils.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Utils.cs" company="None">
+//     (c) 2015 IIHOSHI Yoshinori
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ReimuPlugins.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Provides static methods for convenience.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Gets the <c>IEnumerable{T}</c> instance to enumerate values of the <typeparamref name="TEnum"/>
+        /// type.
+        /// </summary>
+        /// <typeparam name="TEnum">The enumeration type.</typeparam>
+        /// <returns>
+        /// The <c>IEnumerable{T}</c> instance to enumerate values of the <typeparamref name="TEnum"/> type.
+        /// </returns>
+        [CLSCompliant(false)]
+        public static IEnumerable<TEnum> GetEnumerator<TEnum>()
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            return Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+        }
+    }
+}
