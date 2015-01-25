48eb39eef6a28711833cdb9976bf33fb1ad339c8
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-25 21:39:02 +0900

    Common: Add the Win32Window class

---
+++ Common/Win32Window.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Win32Window.cs" company="None">
+//     (c) 2015 IIHOSHI Yoshinori
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ReimuPlugins.Common
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Converter from a <c>HWND</c> value to a <see cref="System.Windows.IWin32Window"/> instance.
+    /// </summary>
+    public class Win32Window : IWin32Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Win32Window"/> class.
+        /// </summary>
+        /// <param name="handle">The handle (the value of <c>HWND</c> type of Win32 API).</param>
+        public Win32Window(IntPtr handle)
+        {
+            this.Handle = handle;
+        }
+
+        /// <summary>
+        /// Gets the window handle for the current instance.
+        /// </summary>
+        public IntPtr Handle { get; private set; }
+    }
+}
