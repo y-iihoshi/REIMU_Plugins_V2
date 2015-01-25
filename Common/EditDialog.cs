d7985f44def6555903c10df8ed7fe74cd4873b54
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-01-25 21:36:52 +0900

    Common: Add the EditDialog class

---
+++ Common/EditDialog.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EditDialog.cs" company="None">
+//     (c) 2015 IIHOSHI Yoshinori
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ReimuPlugins.Common
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Provides an edit dialog.
+    /// </summary>
+    public partial class EditDialog : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditDialog"/> class.
+        /// </summary>
+        public EditDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets a content of the textbox on the dialog.
+        /// </summary>
+        public string Content
+        {
+            get { return this.textEdit.Text; }
+            set { this.textEdit.Text = value; }
+        }
+    }
+}
