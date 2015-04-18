27694fbbf8950e9c45173b7935d2a4d0dc96e8a6
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-04-19 02:29:00 +0900

    Th095Bestshot: Modify to read bitmap data

--- Th095Bestshot/Th095BestshotData.cs
+++ Th095Bestshot/Th095BestshotData.cs
@@ -9,8 +9,10 @@
 namespace ReimuPlugins.Th095Bestshot
 {
     using System;
-    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
     using System.IO;
+    using System.Runtime.InteropServices;
     using ReimuPlugins.Common;
 
     public sealed class Th095BestshotData
@@ -25,6 +27,7 @@ namespace ReimuPlugins.Th095Bestshot
             this.Score = 0;
             this.SlowRate = 0;
             this.CardName = string.Empty;
+            this.Bitmap = null;
         }
 
         public string Signature { get; private set; }
@@ -43,7 +46,14 @@ namespace ReimuPlugins.Th095Bestshot
 
         public string CardName { get; private set; }
 
+        public Bitmap Bitmap { get; private set; }
+
         public void Read(Stream input)
+        {
+            this.Read(input, false);
+        }
+
+        public void Read(Stream input, bool withBitmap)
         {
             using (var reader = new BinaryReader(input))
             {
@@ -57,14 +67,50 @@ namespace ReimuPlugins.Th095Bestshot
                 this.Score = reader.ReadInt32();
                 this.SlowRate = reader.ReadSingle();
                 this.CardName = Enc.CP932.GetString(reader.ReadBytes(0x50));
+
+                if (withBitmap)
+                {
+                    this.Bitmap = ReadBitmap(input, this.Width, this.Height);
+                }
             }
         }
 
         public void Read(string path)
         {
+            this.Read(path, false);
+        }
+
+        public void Read(string path, bool withBitmap)
+        {
             using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
             {
-                this.Read(stream);
+                this.Read(stream, withBitmap);
+            }
+        }
+
+        private static Bitmap ReadBitmap(Stream input, int width, int height)
+        {
+            using (var extracted = new MemoryStream())
+            {
+                Lzss.Extract(input, extracted);
+                extracted.Seek(0, SeekOrigin.Begin);
+
+                using (var bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb))
+                {
+                    using (var locked = new BitmapLock(bitmap, ImageLockMode.WriteOnly))
+                    {
+                        var source = extracted.ToArray();
+                        var sourceStride = 3 * width;   // "3" means 24bpp.
+                        var destination = locked.Scan0;
+                        for (var index = 0; index < source.Length; index += sourceStride)
+                        {
+                            Marshal.Copy(source, index, destination, sourceStride);
+                            destination = new IntPtr(destination.ToInt32() + locked.Stride);
+                        }
+                    }
+
+                    return bitmap.Clone() as Bitmap;
+                }
             }
         }
     }
