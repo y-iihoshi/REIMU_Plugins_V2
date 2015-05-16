6c561df7236f191e6c6ffd818976c4d0f5558612
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2015-05-16 15:03:12 +0900

    Implement Th143Screenshot plugin

---
+++ Th143Screenshot/Plugin.cs
@@ -0,0 +1,488 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Plugin.cs" company="None">
+//     (c) 2015 IIHOSHI Yoshinori
+// </copyright>
+//-----------------------------------------------------------------------
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "Reviewed.")]
+
+namespace ReimuPlugins.Th143Screenshot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Drawing.Imaging;
+    using System.Globalization;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using CommonWin32.Bitmaps;
+    using ReimuPlugins.Common;
+    using RGiesecke.DllExport;
+    using IO = System.IO;
+
+    public static class Plugin
+    {
+        private static readonly PluginImpl Impl = new PluginImpl();
+
+        [DllExport]
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "To comply with the REIMU plugin spec.")]
+        public static Revision GetPluginRevision()
+        {
+            return Impl.GetPluginRevision();
+        }
+
+        [DllExport]
+        public static int GetPluginInfo(int index, IntPtr info, uint size)
+        {
+            return Impl.GetPluginInfo(index, info, size);
+        }
+
+        [DllExport]
+        [SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "0#", Justification = "To comply with the REIMU plugin spec.")]
+        public static ErrorCode GetColumnInfo(out IntPtr info)
+        {
+            return Impl.GetColumnInfo(out info);
+        }
+
+        [DllExport]
+        public static uint IsSupported(IntPtr src, uint size)
+        {
+            return Impl.IsSupported(src, size);
+        }
+
+        [DllExport]
+        [SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "2#", Justification = "To comply with the REIMU plugin spec.")]
+        public static ErrorCode GetFileInfoList(IntPtr src, uint size, out IntPtr info)
+        {
+            return Impl.GetFileInfoList(src, size, out info);
+        }
+
+        [DllExport]
+        [SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "2#", Justification = "To comply with the REIMU plugin spec.")]
+        [SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "3#", Justification = "To comply with the REIMU plugin spec.")]
+        public static ErrorCode GetFileInfoImage1(IntPtr src, uint size, out IntPtr dst, out IntPtr info)
+        {
+            return Impl.GetFileInfoImage1(src, size, out dst, out info);
+        }
+
+        private sealed class PluginImpl : ReimuPluginRev2<PluginImpl.ColumnKey>
+        {
+            private static readonly string ValidSignature = "BST3".ToCP932();
+
+            private static readonly string[] PluginInfo =
+            {
+                "REIMU Plug-in For 弾幕アマノジャク スクリーンショット Ver2.00 (C) IIHOSHI Yoshinori, 2015\0"
+                    .ToCP932(),
+                "弾幕アマノジャク スクリーンショット\0".ToCP932(),
+                "sc*.dat\0".ToCP932(),
+                "弾幕アマノジャク スクリーンショットファイル (sc*.dat)\0".ToCP932(),
+            };
+
+            private static readonly Dictionary<ColumnKey, ColumnInfo> Columns =
+                new Dictionary<ColumnKey, ColumnInfo>
+                {
+                    {
+                        ColumnKey.Filename,
+                        new ColumnInfo
+                        {
+                            Title = "ファイル名\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.String,
+                            System = SystemInfoType.Title
+                        }
+                    },
+                    {
+                        ColumnKey.LastWriteDate,
+                        new ColumnInfo
+                        {
+                            Title = "更新日時\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.String,
+                            System = SystemInfoType.LastWriteTime
+                        }
+                    },
+                    {
+                        ColumnKey.Day,
+                        new ColumnInfo
+                        {
+                            Title = "日付\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.String,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.Scene,
+                        new ColumnInfo
+                        {
+                            Title = "シーン\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.Number,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.DayScene,
+                        new ColumnInfo
+                        {
+                            Title = "日付-シーン\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.Number,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.Width,
+                        new ColumnInfo
+                        {
+                            Title = "幅\0".ToCP932(),
+                            Align = TextAlign.Right,
+                            Sort = SortType.Number,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.Height,
+                        new ColumnInfo
+                        {
+                            Title = "高さ\0".ToCP932(),
+                            Align = TextAlign.Right,
+                            Sort = SortType.Number,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.DateTime,
+                        new ColumnInfo
+                        {
+                            Title = "日時\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.String,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.SlowRate,
+                        new ColumnInfo
+                        {
+                            Title = "処理落ち率\0".ToCP932(),
+                            Align = TextAlign.Right,
+                            Sort = SortType.Float,
+                            System = SystemInfoType.String
+                        }
+                    },
+                    {
+                        ColumnKey.FileSize,
+                        new ColumnInfo
+                        {
+                            Title = "ファイルサイズ\0".ToCP932(),
+                            Align = TextAlign.Right,
+                            Sort = SortType.Number,
+                            System = SystemInfoType.FileSize
+                        }
+                    },
+                    {
+                        ColumnKey.Directory,
+                        new ColumnInfo
+                        {
+                            Title = "ディレクトリ\0".ToCP932(),
+                            Align = TextAlign.Left,
+                            Sort = SortType.String,
+                            System = SystemInfoType.Directory
+                        }
+                    },
+                    {
+                        ColumnKey.Sentinel,
+                        new ColumnInfo
+                        {
+                            Title = "\0",
+                            Align = TextAlign.Left,
+                            Sort = SortType.String,
+                            System = SystemInfoType.String
+                        }
+                    }
+                };
+
+            private static readonly string[] DayStrings =
+            {
+                "一日目", "二日目", "三日目", "四日目", "五日目",
+                "六日目", "七日目", "八日目", "九日目", "最終日"
+            };
+
+            private static readonly Dictionary<ColumnKey, Func<ScreenshotData, string>> FileInfoGetters =
+                InitializeFileInfoGetters();
+
+            internal enum ColumnKey
+            {
+                Filename = 0,
+                LastWriteDate,
+                Day,
+                Scene,
+                DayScene,
+                Width,
+                Height,
+                DateTime,
+                SlowRate,
+                FileSize,
+                Directory,
+                Sentinel
+            }
+
+            protected override ReadOnlyCollection<string> ManagedPluginInfo
+            {
+                get { return Array.AsReadOnly(PluginInfo); }
+            }
+
+            protected override IDictionary<PluginImpl.ColumnKey, ColumnInfo> ManagedColumnInfo
+            {
+                get { return Columns; }
+            }
+
+            public override uint IsSupported(IntPtr src, uint size)
+            {
+                if (src == IntPtr.Zero)
+                {
+                    return (uint)ValidSignature.Length;
+                }
+
+                var signature = string.Empty;
+
+                try
+                {
+                    using (var pair = ReimuPluginRev1<ColumnKey>.CreateStream(src, size))
+                    {
+                        if (pair.Item1 == ErrorCode.AllRight)
+                        {
+                            var reader = new IO.BinaryReader(pair.Item2);
+                            var readSize = Math.Min((int)reader.BaseStream.Length, ValidSignature.Length);
+                            signature = Enc.CP932.GetString(reader.ReadBytes(readSize));
+                        }
+                    }
+                }
+                catch (OutOfMemoryException)
+                {
+                }
+                catch (ArgumentException)
+                {
+                }
+                catch (IO.IOException)
+                {
+                }
+                catch (NotSupportedException)
+                {
+                }
+                catch (ObjectDisposedException)
+                {
+                }
+
+                return (signature == ValidSignature) ? 1u : 0u;
+            }
+
+            public override ErrorCode GetFileInfoList(IntPtr src, uint size, out IntPtr info)
+            {
+                var errorCode = ErrorCode.UnknownError;
+
+                info = IntPtr.Zero;
+
+                try
+                {
+                    var pair = CreateScreenshotData(src, size, false);
+                    if (pair.Item1 == ErrorCode.AllRight)
+                    {
+                        var fileInfoSize = Marshal.SizeOf(typeof(FileInfo));
+                        var keys = Utils.GetEnumerator<ColumnKey>().Where(key => key != ColumnKey.Sentinel);
+
+                        info = Marshal.AllocHGlobal(fileInfoSize * keys.Count());
+
+                        var address = info.ToInt64();
+                        foreach (var key in keys)
+                        {
+                            var fileInfo = new FileInfo { Text = string.Empty };
+                            Func<ScreenshotData, string> getter;
+                            if (FileInfoGetters.TryGetValue(key, out getter))
+                            {
+                                fileInfo.Text = getter(pair.Item2);
+                            }
+
+                            var pointer = new IntPtr(address);
+                            Marshal.StructureToPtr(fileInfo, pointer, false);
+                            address += fileInfoSize;
+                        }
+                    }
+
+                    errorCode = pair.Item1;
+                }
+                catch (OutOfMemoryException)
+                {
+                    errorCode = ErrorCode.NoMemory;
+                }
+                catch (ArgumentException)
+                {
+                }
+                catch (OverflowException)
+                {
+                }
+                finally
+                {
+                    if (errorCode != ErrorCode.AllRight)
+                    {
+                        Marshal.FreeHGlobal(info);
+                        info = IntPtr.Zero;
+                    }
+                }
+
+                return errorCode;
+            }
+
+            public override ErrorCode GetFileInfoText1(IntPtr src, uint size, out IntPtr dst)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override ErrorCode GetFileInfoText2(IntPtr src, uint size, out IntPtr dst)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override ErrorCode GetFileInfoImage1(IntPtr src, uint size, out IntPtr dst, out IntPtr info)
+            {
+                var errorCode = ErrorCode.UnknownError;
+
+                dst = IntPtr.Zero;
+                info = IntPtr.Zero;
+
+                try
+                {
+                    var pair = CreateScreenshotData(src, size, true);
+                    if (pair.Item1 == ErrorCode.AllRight)
+                    {
+                        var copySize = 4 * pair.Item2.Width * pair.Item2.Height;
+
+                        dst = Marshal.AllocHGlobal(copySize);
+
+                        using (var locked = new BitmapLock(pair.Item2.Bitmap, ImageLockMode.ReadOnly))
+                        {
+                            var copy = new byte[copySize];
+                            Marshal.Copy(locked.Scan0, copy, 0, copy.Length);
+                            Marshal.Copy(copy, 0, dst, copy.Length);
+                        }
+
+                        var bitmapInfo = new BITMAPINFO
+                        {
+                            bmiHeader = new BITMAPINFOHEADER
+                            {
+                                biSize = (uint)Marshal.SizeOf(typeof(BITMAPINFOHEADER)),
+                                biWidth = pair.Item2.Bitmap.Width,
+                                biHeight = -pair.Item2.Bitmap.Height,
+                                biPlanes = 1,
+                                biBitCount = 32,
+                                biCompression = BITMAPINFOHEADER.CompressionType.BI_RGB,
+                                biSizeImage = 0,
+                                biXPelsPerMeter = 0,
+                                biYPelsPerMeter = 0,
+                                biClrUsed = 0,
+                                biClrImportant = 0,
+                            },
+                            bmiColors = IntPtr.Zero,
+                        };
+
+                        info = Marshal.AllocHGlobal(Marshal.SizeOf(bitmapInfo));
+                        Marshal.StructureToPtr(bitmapInfo, info, true);
+                    }
+
+                    errorCode = pair.Item1;
+                }
+                catch (OutOfMemoryException)
+                {
+                    errorCode = ErrorCode.NoMemory;
+                }
+                catch (ArgumentException)
+                {
+                }
+                finally
+                {
+                    if (errorCode != ErrorCode.AllRight)
+                    {
+                        Marshal.FreeHGlobal(dst);
+                        dst = IntPtr.Zero;
+                        Marshal.FreeHGlobal(info);
+                        info = IntPtr.Zero;
+                    }
+                }
+
+                return errorCode;
+            }
+
+            public override ErrorCode GetFileInfoImage2(IntPtr src, uint size, out IntPtr dst, out IntPtr info)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override ErrorCode EditDialog(IntPtr parent, string file)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override ErrorCode ConfigDialog(IntPtr parent)
+            {
+                throw new NotImplementedException();
+            }
+
+            [SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity", Justification = "Reviewed.")]
+            private static Dictionary<ColumnKey, Func<ScreenshotData, string>> InitializeFileInfoGetters()
+            {
+                return new Dictionary<ColumnKey, Func<ScreenshotData, string>>
+                {
+                    {
+                        ColumnKey.Day,
+                        (data) => DayStrings[data.Day].ToCP932()
+                    },
+                    {
+                        ColumnKey.Scene,
+                        (data) => (data.Scene + 1).ToString(CultureInfo.CurrentCulture)
+                    },
+                    {
+                        ColumnKey.DayScene,
+                        (data) => string.Format(
+                            CultureInfo.CurrentCulture, "{0}-{1}", data.Day + 1, data.Scene + 1)
+                    },
+                    {
+                        ColumnKey.Width,
+                        (data) => data.Width.ToString(CultureInfo.CurrentCulture)
+                    },
+                    {
+                        ColumnKey.Height,
+                        (data) => data.Height.ToString(CultureInfo.CurrentCulture)
+                    },
+                    {
+                        ColumnKey.DateTime,
+                        (data) => new DateTime(1970, 1, 1).AddSeconds(data.DateTime).ToLocalTime()
+                            .ToString("yyyy/MM/dd HH:mm:ss", CultureInfo.CurrentCulture)
+                    },
+                    {
+                        ColumnKey.SlowRate,
+                        (data) => data.SlowRate.ToString("F6", CultureInfo.CurrentCulture)
+                    },
+                };
+            }
+
+            private static Tuple<ErrorCode, ScreenshotData> CreateScreenshotData(
+                IntPtr src, uint size, bool withBitmap)
+            {
+                using (var pair = ReimuPluginRev1<ColumnKey>.CreateStream(src, size))
+                {
+                    ScreenshotData screenshot = null;
+
+                    if (pair.Item1 == ErrorCode.AllRight)
+                    {
+                        screenshot = new ScreenshotData();
+                        screenshot.Read(pair.Item2, withBitmap);
+                    }
+
+                    return Tuple.Create(pair.Item1, screenshot);
+                }
+            }
+        }
+    }
+}
---
+++ Th143Screenshot/ScreenshotData.cs
@@ -0,0 +1,112 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BestshotData.cs" company="None">
+//     (c) 2015 IIHOSHI Yoshinori
+// </copyright>
+//-----------------------------------------------------------------------
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "Reviewed.")]
+
+namespace ReimuPlugins.Th143Screenshot
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using ReimuPlugins.Common;
+
+    public sealed class ScreenshotData
+    {
+        public ScreenshotData()
+        {
+            this.Signature = string.Empty;
+            this.Day = 0;
+            this.Scene = 0;
+            this.Width = 0;
+            this.Height = 0;
+            this.DateTime = 0;
+            this.SlowRate = 0;
+            this.Bitmap = null;
+        }
+
+        public string Signature { get; private set; }
+
+        public short Day { get; private set; }
+
+        public short Scene { get; private set; }
+
+        public short Width { get; private set; }
+
+        public short Height { get; private set; }
+
+        public uint DateTime { get; private set; }
+
+        public float SlowRate { get; private set; }
+
+        public Bitmap Bitmap { get; private set; }
+
+        public void Read(Stream input)
+        {
+            this.Read(input, false);
+        }
+
+        public void Read(Stream input, bool withBitmap)
+        {
+            using (var reader = new BinaryReader(input))
+            {
+                this.Signature = Enc.CP932.GetString(reader.ReadBytes(4));
+                reader.ReadInt16();
+                this.Day = reader.ReadInt16();
+                this.Scene = reader.ReadInt16();
+                reader.ReadInt16();
+                this.Width = reader.ReadInt16();
+                this.Height = reader.ReadInt16();
+                reader.ReadInt32();
+                this.DateTime = reader.ReadUInt32();
+                this.SlowRate = reader.ReadSingle();
+                reader.ReadBytes(0x58);
+
+                if (withBitmap)
+                {
+                    this.Bitmap = ReadBitmap(input, this.Width, this.Height);
+                }
+            }
+        }
+
+        public void Read(string path)
+        {
+            this.Read(path, false);
+        }
+
+        public void Read(string path, bool withBitmap)
+        {
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                this.Read(stream, withBitmap);
+            }
+        }
+
+        private static Bitmap ReadBitmap(Stream input, int width, int height)
+        {
+            using (var extracted = new MemoryStream())
+            {
+                Lzss.Extract(input, extracted);
+                extracted.Seek(0, SeekOrigin.Begin);
+
+                using (var bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb))
+                {
+                    using (var locked = new BitmapLock(bitmap, ImageLockMode.WriteOnly))
+                    {
+                        var source = extracted.ToArray();
+                        var destination = locked.Scan0;
+                        Marshal.Copy(source, 0, destination, source.Length);
+                    }
+
+                    return bitmap.Clone() as Bitmap;
+                }
+            }
+        }
+    }
+}
