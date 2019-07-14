24eef9eaad2c423d3b6a6c230ea2103b4d479568
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2019-07-14 20:49:16 +0900

    Add a custom stylesheet for the manual

--- ManualGenerator/conf.py
+++ ManualGenerator/conf.py
@@ -289,3 +289,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+def setup(app):
+    app.add_stylesheet('custom.css')
