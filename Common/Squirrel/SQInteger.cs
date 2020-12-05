196889a785d081c58acc47244f7cfcf40f1f3ff5
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-12-06 00:05:00 +0900

    Remove unnecessary suppressions of SA1600

--- Common/Squirrel/SQInteger.cs
+++ Common/Squirrel/SQInteger.cs
@@ -6,7 +6,6 @@
 //-----------------------------------------------------------------------
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-#pragma warning disable SA1600 // Elements should be documented
 
 namespace ReimuPlugins.Common.Squirrel
 {
