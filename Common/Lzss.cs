8554c62cceb1407599e435280b4212f2f8ce2813
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2020-12-05 23:33:35 +0900

    Remove unnecessary suppressions of CA1801

--- Common/Lzss.cs
+++ Common/Lzss.cs
@@ -27,7 +27,6 @@ namespace ReimuPlugins.Common
         /// </summary>
         /// <param name="input">The stream to input data.</param>
         /// <param name="output">The stream that is output the compressed data.</param>
-        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", Justification = "Reviewed.")]
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "For future use.")]
 #pragma warning disable IDE0060 // Remove unused parameter
         public static void Compress(Stream input, Stream output)
