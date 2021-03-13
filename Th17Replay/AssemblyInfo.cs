80fc2d59ec104fef4ba089cd55d4b55e656ce9a7
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2021-03-13 18:22:57 +0900

    Fix to avoid CA1014

---
+++ Th17Replay/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="None">
+// Copyright (c) IIHOSHI Yoshinori.
+// Licensed under the BSD-2-Clause license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6e02ccc0-7e61-4e26-ac50-2128fc8c1b72")]
+
+[assembly: CLSCompliant(false)]
