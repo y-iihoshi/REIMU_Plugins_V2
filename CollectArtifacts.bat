fe29a8ac57438472c3336332a60f22db8edb6992
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2023-08-06 16:57:57 +0900

    Fix to upload artifact of debug build

--- CollectArtifacts.bat
+++ CollectArtifacts.bat
@@ -10,19 +10,24 @@ if "%target_framework%" == "" (
     exit /b 1
 )
 
-if not exist publish\%target_framework%\plugin (
-    mkdir publish\%target_framework%\plugin
+set output_dir=%~3
+if "%output_dir%" == "" (
+    set output_dir=publish\%configuration%\%target_framework%
+)
+
+if not exist %output_dir%\plugin (
+    mkdir %output_dir%\plugin
 )
 
 for %%f in (Common\bin\%configuration%\%target_framework%\*.dll) do (
-    copy /b /y %%f publish\%target_framework% > NUL
+    copy /b /y %%f %output_dir% > NUL
 )
 
 for /d %%d in (Th*) do (
     if not "%%d" == "Th11Replay" (
         copy /b /y %%d\bin\%configuration%\%target_framework%\ReimuPlugins.%%d.dll ^
-            publish\%target_framework%\plugin\ReimuPlugins.%%d.rpi > NUL
+            %output_dir%\plugin\ReimuPlugins.%%d.rpi > NUL
     )
 )
 
-xcopy ManualGenerator\_build\html publish\%target_framework%\doc /e /i /q
+xcopy ManualGenerator\_build\html %output_dir%\doc /e /i /q
