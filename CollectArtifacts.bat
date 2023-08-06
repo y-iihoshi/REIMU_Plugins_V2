d03ab4e59a21456af20808285e258ac69af27b60
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2023-08-06 22:26:56 +0900

    Revert the changes reated to collect-artifacts

--- CollectArtifacts.bat
+++ CollectArtifacts.bat
@@ -10,24 +10,19 @@ if "%target_framework%" == "" (
     exit /b 1
 )
 
-set output_dir=%~3
-if "%output_dir%" == "" (
-    set output_dir=publish\%configuration%\%target_framework%
-)
-
-if not exist %output_dir%\plugin (
-    mkdir %output_dir%\plugin
+if not exist publish\%target_framework%\plugin (
+    mkdir publish\%target_framework%\plugin
 )
 
 for %%f in (Common\bin\%configuration%\%target_framework%\*.dll) do (
-    copy /b /y %%f %output_dir% > NUL
+    copy /b /y %%f publish\%target_framework% > NUL
 )
 
 for /d %%d in (Th*) do (
     if not "%%d" == "Th11Replay" (
         copy /b /y %%d\bin\%configuration%\%target_framework%\ReimuPlugins.%%d.dll ^
-            %output_dir%\plugin\ReimuPlugins.%%d.rpi > NUL
+            publish\%target_framework%\plugin\ReimuPlugins.%%d.rpi > NUL
     )
 )
 
-xcopy ManualGenerator\_build\html %output_dir%\doc /e /i /q
+xcopy ManualGenerator\_build\html publish\%target_framework%\doc /e /i /q
